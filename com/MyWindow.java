84784ff231d6a0a3620e63144ccb313d4e7c6e4f
Abhishek <abhisam7007871@gmail.com>
2023-12-25 22:09:37 +0530

    [feature]Digital Clock

---
+++ com/MyWindow.java
@@ -0,0 +1,34 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyWindow extends JFrame {
+    private JLabel heading;
+    private JLabel clockLabel;
+
+    private Font font = new Font("", Font.BOLD, 35);
+    MyWindow(){
+        super.setTitle("My Clock");
+        super.setSize(400,400);
+        super.setLocation(300,200);
+        this.creatGUI();
+        super.setVisible(true);
+    }
+
+    public void creatGUI(){
+        heading = new JLabel("My Clock");
+
+        clockLabel = new JLabel("clock");
+
+        heading.setFont(font);
+        clockLabel.setFont(font);
+
+
+
+        this.setLayout(new GridLayout(2,1));
+        this.add(heading);
+        this.add(clockLabel);
+    }
+
+}
