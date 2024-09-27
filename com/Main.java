babb137e206f0fb109dbbd7b1670b752f04b1564
Abhishek Sharma <102051093+Abhisam7007871@users.noreply.github.com>
2024-09-27 16:47:48 +0530

    Update Main.java

--- com/Main.java
+++ com/Main.java
@@ -3,7 +3,9 @@ package com;
 public class Main
 {
     public static void main(String[] args) {
+
         System.out.println("My Clock by Abhishek");
+        
         MyWindow w = new MyWindow();
 
     }
